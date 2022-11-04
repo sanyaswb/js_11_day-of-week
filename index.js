84ddf9114cd716b5807019d14cfef3e9f73ecc46
sanyaswb <sanyaswb@gmail.com>
2022-11-04 14:28:06 +0200

    11_day-of-week

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 function dayOfWeek(day) {
-	// write your code here
+	switch (day)  {
+		case 1:
+			return 'sunday';
+		case 2:
+			return 'monday';
+		case 3:
+			return 'tuesday';
+		case 4:
+			return 'wednesday';
+		case 5:
+			return 'thursday';
+		case 6:
+			return 'friday';
+		case 7:
+			return 'saturday';
+		default:
+			return '';
+	}
 }
 
 module.exports = dayOfWeek;
